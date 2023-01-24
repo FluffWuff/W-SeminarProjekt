07a1407976fd02ae4c3fd09cc4cb1a9d800c7b9f
FluffWuff <alexderrichtiger@gmail.com>
2023-01-24 15:12:50 +0100

    Overflow

--- src/client/level/Levels.ts
+++ src/client/level/Levels.ts
@@ -1,5 +1,5 @@
 import { Button } from "../util/Button.js"
-import { DISTANCE_MA_RO, GR_START_POS_X, GR_START_POS_Y, MA_HEIGHT, MA_START_POS_X, MA_START_POS_Y, MA_PRIMARY_COLOR, MA_WIDH, RO_HEIGHT, RO_WIDTH } from "../util/Constants.js"
+import { DISTANCE_MA_RO, GR_START_POS_X, GR_START_POS_Y, MA_HEIGHT, MA_START_POS_X, MA_START_POS_Y, MA_PRIMARY_COLOR, MA_WIDH, RO_HEIGHT, RO_WIDTH, DISTANCE_MA_OV_X, DISTANCE_MA_OV_Y, OV_COLOR } from "../util/Constants.js"
 
 type MemoryAddress = Button
 
@@ -7,14 +7,36 @@ export class GameLevel {
 
     public currentTimer: number
     public grid: MemoryAddress[][]
-
+    
     public routines: MemoryAddress[][] = []
+
+    private currentOverflow = 0
+    private overflowText: Phaser.GameObjects.Text
 
     constructor(private scene: Phaser.Scene, private levelConfig: GameLevelConfig) {
         this.grid = []
 
         this.fillGridUp()
         this.createRoutines()
+        this.createOverflow()
+        this.createTimer()
+        this.createDetails()
+    }
+
+    private fillGridUp() {
+        let gridText = this.scene.add.text(MA_START_POS_X*this.scene.cameras.main.centerX*2, MA_START_POS_Y*this.scene.cameras.main.centerY*2, "memory matrix", {
+            align: 'left',
+            font: '24px DS-DIGII',
+            //backgroundColor: MA_BACKGROUND_COLOR,
+            color: "#"+MA_PRIMARY_COLOR.toString(16)
+        })
+        for(var i = 0; i < this.levelConfig.rows; i++) {
+            this.grid[i] = []
+            for(var j = 0; j < this.levelConfig.columns; j++) {
+                  this.grid[i][j] = this.createMemoryAddress(MA_WIDH*j*1.025+MA_START_POS_X*this.scene.cameras.main.centerX*2,
+                     MA_HEIGHT*i*1.025+MA_START_POS_Y*this.scene.cameras.main.centerY*2+25, false)
+            }
+        }
     }
 
     private createRoutines() {
@@ -56,20 +78,27 @@ export class GameLevel {
         
     }
 
-    private fillGridUp() {
-        let gridText = this.scene.add.text(MA_START_POS_X*this.scene.cameras.main.centerX*2, MA_START_POS_Y*this.scene.cameras.main.centerY*2, "memory matrix", {
+    private createOverflow() {
+        this.overflowText = this.scene.add.text(MA_START_POS_X*this.scene.cameras.main.centerX*2+DISTANCE_MA_OV_X,
+            MA_START_POS_Y*this.scene.cameras.main.centerY*2-DISTANCE_MA_OV_Y, "overflow ("+this.currentOverflow+"/"+this.levelConfig.maxOverflow+")", {
             align: 'left',
-            font: '24px DS-DIGII',
+            font: '20px DS-DIGII',
             //backgroundColor: MA_BACKGROUND_COLOR,
-            color: "#"+MA_PRIMARY_COLOR.toString(16)
+            color: "#"+OV_COLOR.toString(16)
         })
-        for(var i = 0; i < this.levelConfig.rows; i++) {
-            this.grid[i] = []
-            for(var j = 0; j < this.levelConfig.columns; j++) {
-                  this.grid[i][j] = this.createMemoryAddress(MA_WIDH*j*1.025+MA_START_POS_X*this.scene.cameras.main.centerX*2,
-                     MA_HEIGHT*i*1.025+MA_START_POS_Y*this.scene.cameras.main.centerY*2+25, false)
-            }
-        }
+        
+    }
+
+    private updateOverflow() {
+        this.overflowText.setText("overflow ("+this.currentOverflow+"/"+this.levelConfig.maxOverflow+")")
+    }
+
+    private createTimer() {
+        
+    }
+
+    private createDetails() {
+
     }
 
     private createMemoryAddress(x: number, y: number, isSmall: boolean, text?: string): MemoryAddress {
@@ -92,7 +121,7 @@ export type GameLevelConfig = {
     rows: number,
     columns: number,
     maxTime: number,
-    maxErros: number,
+    maxOverflow: number,
     minRoutines: number, 
     maxRoutines: number,
     minRoutineLength: number,
--- src/client/util/Constants.ts
+++ src/client/util/Constants.ts
@@ -4,7 +4,7 @@ import { GameLevelConfig } from "../level/Levels.js"
 export const greenPrimaryColor = 0x82f676
 export const greenSecondaryColor = 0x102210
 export const yellowColor = 0xa6a666
-
+export const whiteColor = 0xFFFFFF
 
 //MemoryAddress/Spielfeld:
 export const MA_WIDH = 64
@@ -17,22 +17,29 @@ export const MA_BACKGROUND_64 = 'memoryaddress_background_64'
 export const MA_START_POS_X = 0.3
 export const MA_START_POS_Y = 0.3
 
+//Distanz zwischen Spielfeld und Routinen X-Achse:
 export const DISTANCE_MA_RO = 60
 
 //Spielfeld:
 export const GR_START_POS_X = 40
 export const GR_START_POS_Y = 20
 
-//Routinen
+//Routinen WIDTH - HEIGHT
 export const RO_WIDTH = 32
 export const RO_HEIGHT = 32
+
+//Overflow
+export const DISTANCE_MA_OV_X = 150
+export const DISTANCE_MA_OV_Y = 150
+export const OV_COLOR = whiteColor
+
 
 //Levelkonfigurationen:
 export const LEVEL_2_GAME_LEVEL_CONFIG: GameLevelConfig = {
     rows: 3,
     columns: 4,
     maxTime: 120,
-    maxErros: 5,
+    maxOverflow: 5,
     minRoutines: 2, 
     maxRoutines: 3,
     minRoutineLength: 2,
@@ -42,7 +49,7 @@ export const LEVEL_3_GAME_LEVEL_CONFIG: GameLevelConfig = {
     rows: 4,
     columns: 5,
     maxTime: 120,
-    maxErros: 4,
+    maxOverflow: 4,
     minRoutines: 3, 
     maxRoutines: 4,
     minRoutineLength: 2,
@@ -53,7 +60,7 @@ export const LEVEL_4_GAME_LEVEL_CONFIG: GameLevelConfig = {
     rows: 5,
     columns: 4,
     maxTime: 120,
-    maxErros: 4,
+    maxOverflow: 4,
     minRoutines: 3, 
     maxRoutines: 4,
     minRoutineLength: 2,
@@ -63,7 +70,7 @@ export const LEVEL_5_GAME_LEVEL_CONFIG: GameLevelConfig = {
     rows: 5,
     columns: 4,
     maxTime: 120,
-    maxErros: 4,
+    maxOverflow: 4,
     minRoutines: 3, 
     maxRoutines: 4,
     minRoutineLength: 2,
