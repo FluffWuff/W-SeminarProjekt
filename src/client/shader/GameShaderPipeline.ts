832889dcb46c93e510f7021b46dc2d45a79484a1
FluffWuff <alexderrichtiger@gmail.com>
2023-08-10 09:51:35 +0200

    finalised shader

--- src/client/shader/GameShaderPipeline.ts
+++ src/client/shader/GameShaderPipeline.ts
@@ -2,17 +2,19 @@ const fragShader = `
 #define SHADER_NAME HUE_ROTATE_FS
 precision mediump float;
 uniform sampler2D uMainSampler;
+uniform float uTime;
+uniform float uSpeed;
 varying vec2 outTexCoord;
 void main() {
     vec3 color = vec3(0.0, 0.0, 0.0);
     
-    float gx = sin(outTexCoord.x*1920.);
-    float gy = cos(outTexCoord.y*1080.);
-    if(gx<0.967) { 
+    float gx = sin(outTexCoord.x*1920.0);
+    float gy = cos(outTexCoord.y*1080.0 + uTime*.05);
+    if(gy<0.967 && gx < 0.967) { 
         gl_FragColor = texture2D(uMainSampler, outTexCoord);
     } else {
-        color = vec3(0, 0.6-gy-gx, 0.12);;
-        gl_FragColor = vec4(color, .2);
+        color = vec3(0, 0.4-gy-gx, 0.12);;
+        gl_FragColor = vec4(color, 0.002);
     }
 }
 `;
@@ -25,14 +27,16 @@ export class GameDefaultShaderPipeline extends Phaser.Renderer.WebGL.Pipelines.P
             name: "GameDefaultShaderPipeline",
             //@ts-ignore
             uniforms: [
-                'uMainSampler'
+                'uMainSampler',
+                'uTime'
             ],
             fragShader: fragShader
         })
     }
 
     onPreRender(): void {
-
+        this.setTime('uTime');
+        this.set1f('uSpeed',1);
     }
 
 }
