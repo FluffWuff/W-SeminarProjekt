bad1fa612634c3c80ce89f216c57b0088fc3a87c
RichtigerAlex <61325844+FluffWuff@users.noreply.github.com>
2023-10-20 19:12:51 +0200

    changed some things

--- src/client/scenes/SelectionScene.ts
+++ src/client/scenes/SelectionScene.ts
@@ -8,25 +8,21 @@ export class SelectionScene extends Phaser.Scene {
         })
     }
 
-    init() {
-
-    }
-
     preload() {
         this.load.glsl('matrixshader', 'assets/shader/shader.frag')
     }
 
     create() {
-        let level1Box = new SelectionButton(this, 1920 / 2, 200, "Level 1", LEVEL_1_GAME_LEVEL_CONFIG)
-        let level2Box = new SelectionButton(this, 1920 / 2, 300, "Level 2", LEVEL_2_GAME_LEVEL_CONFIG)
-        let level3Box = new SelectionButton(this, 1920 / 2, 400, "Level 3", LEVEL_3_GAME_LEVEL_CONFIG)
-        let level4Box = new SelectionButton(this, 1920 / 2, 500, "Level 4", LEVEL_4_GAME_LEVEL_CONFIG)
-        let level5Box = new SelectionButton(this, 1920 / 2, 600, "Level 5", LEVEL_5_GAME_LEVEL_CONFIG)
+        let level1Box = new LevelSelectionButton(this, 1920 / 2, 200, "Level 1", LEVEL_1_GAME_LEVEL_CONFIG)
+        let level2Box = new LevelSelectionButton(this, 1920 / 2, 300, "Level 2", LEVEL_2_GAME_LEVEL_CONFIG)
+        let level3Box = new LevelSelectionButton(this, 1920 / 2, 400, "Level 3", LEVEL_3_GAME_LEVEL_CONFIG)
+        let level4Box = new LevelSelectionButton(this, 1920 / 2, 500, "Level 4", LEVEL_4_GAME_LEVEL_CONFIG)
+        let level5Box = new LevelSelectionButton(this, 1920 / 2, 600, "Level 5", LEVEL_5_GAME_LEVEL_CONFIG)
     }
 
 }
 
-class SelectionButton extends Phaser.GameObjects.Group {
+class LevelSelectionButton extends Phaser.GameObjects.Group {
 
     constructor(scene: Phaser.Scene, x: number, y: number, text: string, levelGameConfig: GameLevelConfig) {
         super(scene)
@@ -40,19 +36,21 @@ class SelectionButton extends Phaser.GameObjects.Group {
             align: 'center',
             font: '64px DS-DIGII',
         })
+
         this.add(textElement)
         this.add(rectangle)
+        
         textElement.setTint(parseInt(MA_PRIMARY_COLOR.toString(16), 16))
 
-        textElement.setInteractive().on('pointerover', (pointer, localX, localY, event) => {
+        rectangle.setInteractive().on('pointerover', (pointer, localX, localY, event) => {
             rectangle.strokeColor = MA_SELECTED_COLOR
             rectangle.setFillStyle(MA_HIDE_SELECTED_COLOR)
             textElement.setTint(MA_SELECTED_COLOR)
         })
-        textElement.setInteractive().on('pointerdown', (pointer, localX, localY, event) => {
+        rectangle.setInteractive().on('pointerdown', (pointer, localX, localY, event) => {
             this.scene.scene.start("GameScene", levelGameConfig)
         })
-        textElement.setInteractive().on('pointerout', (pointer, localX, localY, event) => {
+        rectangle.setInteractive().on('pointerout', (pointer, localX, localY, event) => {
             rectangle.strokeColor = MA_PRIMARY_COLOR
             rectangle.setFillStyle(MA_HIDE_COLOR)
             textElement.setTint(MA_PRIMARY_COLOR)
